d17b7389475266eb044d4ca2076041cf145f5e7b
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2024-01-18 17:29:54 -0300

    Concluído o Menu e as rotas

---
+++ src/pages/header.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+function Header() {
+    return (
+        <div className="div_header">
+            <div class="float-container">
+
+                <div class="div_logo">
+                    <img className="logo" src='Images/logo.svg' alt="Logo" />
+                </div>
+
+                <div class="div_links">
+                    <Link className="align_link" to="/"><img className="icon" src="Images/schedule.png" alt="Schedule Icon" />Schedule</Link>
+                    <Link className="align_link" to="/"><img className="icon" src="Images/leaderboard.png" alt="Leaderboard Icon" />Leaderboard</Link>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Header
---
+++ src/pages/schedule.js
@@ -0,0 +1,11 @@
+import Header from "../pages/header";
+function Schedule() {
+    return (
+        <div>
+            <Header />
+            <h1>Schedule</h1>
+        </div>
+    );
+}
+
+export default Schedule
