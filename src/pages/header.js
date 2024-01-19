2a8b357cb8d965d59fceb169ec2c9bbaf7b1868c
Fabrício Dias de Oliveira <devanon.kyosha@gmail.com>
2024-01-19 10:32:04 -0300

    Obtido o token

--- src/pages/header.js
+++ src/pages/header.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 function Header() {
     return (
         <div className="div_header">
-            <div class="float-container">
+            <div className="float-container">
 
-                <div class="div_logo">
+                <div className="div_logo">
                     <img className="logo" src='Images/logo.svg' alt="Logo" />
                 </div>
 
-                <div class="div_links">
+                <div className="div_links">
                     <Link className="align_link" to="/"><img className="icon" src="Images/schedule.png" alt="Schedule Icon" />Schedule</Link>
                     <Link className="align_link" to="/leaderboard"><img className="icon" src="Images/leaderboard.png" alt="Leaderboard Icon" />Leaderboard</Link>
                 </div>
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,9 +1,42 @@
 import Header from "../pages/header";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function Schedule() {
+    const [results, setResults] = useState([])
+    const [access_token, setAccess_token] = useState('')
+    function getAccessToken(){
+        axios.get('http://localhost:3001/api/v1/getAccessToken')
+          .then(resposta => {
+            setAccess_token(resposta.data.access_token);
+          })
+          .catch(erro => {
+            console.log(erro)
+          })
+    }
+    
+    function getSchedule(){
+        axios.get('http://localhost:3001/api/v1/getAllMatches',
+        {
+          headers: {
+            'Authorization': `Bearer ${access_token}`
+          }
+        })
+          .then(resposta => {
+            setResults(resposta.data);
+          })
+          .catch(erro => {
+            console.log(erro)
+          })
+    }
+    useEffect(() => {
+        getAccessToken();
+        getSchedule();
+        console.log(results)
+      }, [])
     return (
         <div>
             <Header />
-            <h1>Schedule</h1>
+            <h1>{access_token}</h1>
         </div>
     );
 }
